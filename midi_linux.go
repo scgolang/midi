421d9910872be36672cd143b2ef98cfe252a38e9
Brian Sorahan <bsorahan@gmail.com>
2017-02-22 21:47:40 -0600

    rewrote Devices func for linux

--- midi_linux.go
+++ midi_linux.go
@@ -12,6 +12,7 @@ import "C"
 import (
 	"fmt"
 	"os"
+	"unsafe"
 
 	"github.com/pkg/errors"
 )
@@ -19,22 +20,36 @@ import (
 // Packet is a MIDI packet.
 type Packet [3]byte
 
+// DeviceType is a flag that says if a device is an input, an output, or duplex.
+type DeviceType int
+
+const (
+	DeviceInput DeviceType = iota
+	DeviceOutput
+	DeviceDuplex
+)
+
 // Device provides an interface for MIDI devices.
 type Device struct {
+	ID        string
 	Name      string
 	QueueSize int
+	Type      DeviceType
 
 	conn C.Midi
-	buf  []byte
 }
 
 // Open opens a MIDI device.
 func (d *Device) Open() error {
-	result := C.Midi_open(C.CString(d.Name))
+	var (
+		id     = C.CString(d.ID)
+		result = C.Midi_open(id)
+	)
 	if result.error != 0 {
 		return errors.Errorf("error opening device %d", result.error)
 	}
 	d.conn = result.midi
+	C.free(unsafe.Pointer(id))
 	return nil
 }
 
@@ -76,7 +91,9 @@ func (d *Device) Read(buf []byte) (int, error) {
 
 // Write writes data to a MIDI device.
 func (d *Device) Write(buf []byte) (int, error) {
-	n, err := C.Midi_write(d.conn, C.CString(string(buf)), C.size_t(len(buf)))
+	cs := C.CString(string(buf))
+	n, err := C.Midi_write(d.conn, cs, C.size_t(len(buf)))
+	C.free(unsafe.Pointer(cs))
 	return int(n), err
 }
 
@@ -84,106 +101,148 @@ type Stream struct {
 	Name string
 }
 
-type DeviceInfo struct {
-	ID      string
-	Name    string
-	Inputs  []Stream
-	Outputs []Stream
-}
+func Devices() ([]Device, error) {
+	var card C.int = -1
 
-func Devices() ([]DeviceInfo, error) {
-	var (
-		seqp  *C.snd_seq_t
-		cinfo *C.snd_seq_client_info_t
-		pinfo *C.snd_seq_port_info_t
-	)
-	if rc := C.snd_seq_open(&seqp, C.CString("default"), C.SND_SEQ_OPEN_DUPLEX, 0); rc != 0 {
+	if rc := C.snd_card_next(&card); rc != 0 {
 		return nil, alsaMidiError(rc)
 	}
-	if rc := C.snd_seq_client_info_malloc(&cinfo); rc != 0 {
-		return nil, alsaMidiError(rc)
+	if card < 0 {
+		return nil, errors.New("no sound card found")
 	}
+	devices := []Device{}
 
-	if rc := C.snd_seq_port_info_malloc(&pinfo); rc != 0 {
-		return nil, alsaMidiError(rc)
-	}
-	C.snd_seq_client_info_set_client(cinfo, -1)
-
-	devices := []DeviceInfo{}
-
-	for C.snd_seq_query_next_client(seqp, cinfo) == 0 {
-		C.snd_seq_port_info_set_client(pinfo, C.snd_seq_client_info_get_client(cinfo))
-		C.snd_seq_port_info_set_port(pinfo, -1)
-
-		if clientID := C.snd_seq_port_info_get_client(pinfo); isSystemClient(clientID) {
-			continue // ignore Timer and Announce ports on client 0
+	for {
+		cardDevices, err := getCardDevices(card)
+		if err != nil {
+			return nil, err
 		}
-		device := DeviceInfo{
-			Name: C.GoString(C.snd_seq_client_info_get_name(cinfo)),
+		if rc := C.snd_card_next(&card); rc != 0 {
+			return nil, alsaMidiError(rc)
 		}
-		if card := C.snd_seq_client_info_get_card(cinfo); card >= 0 {
-			device.ID = fmt.Sprintf("hw:%d", card)
+		if card < 0 {
+			break
 		}
-		for C.snd_seq_query_next_port(seqp, pinfo) == 0 {
-			if portType := C.snd_seq_port_info_get_type(pinfo); !isMidiPort(portType) {
-				fmt.Printf("portType is not a midi port %d\n", portType)
-				continue // Not a MIDI port.
-			}
-			var (
-				caps     = C.snd_seq_port_info_get_capability(pinfo)
-				portName = C.GoString(C.snd_seq_port_info_get_name(pinfo))
-				stream   = Stream{Name: portName}
-			)
-			if isDuplexPort(caps) {
-				// Add duplex stream.
-				device.Inputs = append(device.Inputs, stream)
-				device.Outputs = append(device.Outputs, stream)
-			} else if isWritePort(caps) {
-				// Add write stream.
-				device.Outputs = append(device.Outputs, stream)
-			} else if isReadPort(caps) {
-				// Add read stream.
-				device.Inputs = append(device.Inputs, stream)
-			}
-		}
-		devices = append(devices, device)
+		devices = append(devices, cardDevices...)
 	}
 	return devices, nil
 }
 
-func isSystemClient(clientID C.int) bool {
-	return clientID == C.SND_SEQ_CLIENT_SYSTEM
+func getCardDevices(card C.int) ([]Device, error) {
+	var (
+		ctl  *C.snd_ctl_t
+		name = C.CString(fmt.Sprintf("hw:%d", card))
+	)
+	if rc := C.snd_ctl_open(&ctl, name, 0); rc != 0 {
+		return nil, alsaMidiError(rc)
+	}
+	C.free(unsafe.Pointer(name))
+
+	var (
+		cardDevices       = []Device{}
+		device      C.int = -1
+	)
+	for {
+		if rc := C.snd_ctl_rawmidi_next_device(ctl, &device); rc != 0 {
+			return nil, alsaMidiError(rc)
+		}
+		if device < 0 {
+			break
+		}
+		deviceDevices, err := getDeviceDevices(ctl, card, C.uint(device))
+		if err != nil {
+			return nil, err
+		}
+		cardDevices = append(cardDevices, deviceDevices...)
+	}
+	if rc := C.snd_ctl_close(ctl); rc != 0 {
+		return nil, alsaMidiError(rc)
+	}
+	return cardDevices, nil
 }
 
-func isSystemPort(portID C.int) bool {
-	return portID == C.SND_SEQ_PORT_SYSTEM_ANNOUNCE || portID == C.SND_SEQ_PORT_SYSTEM_TIMER
+func getDeviceDevices(ctl *C.snd_ctl_t, card C.int, device C.uint) ([]Device, error) {
+	var info *C.snd_rawmidi_info_t
+	C.snd_rawmidi_info_malloc(&info)
+	C.snd_rawmidi_info_set_device(info, device)
+
+	// Get inputs.
+	var subsIn C.uint
+	C.snd_rawmidi_info_set_stream(info, C.SND_RAWMIDI_STREAM_INPUT)
+	if rc := C.snd_ctl_rawmidi_info(ctl, info); rc != 0 {
+		return nil, alsaMidiError(rc)
+	}
+	subsIn = C.snd_rawmidi_info_get_subdevices_count(info)
+
+	// Get outputs.
+	var subsOut C.uint
+	C.snd_rawmidi_info_set_stream(info, C.SND_RAWMIDI_STREAM_OUTPUT)
+	if rc := C.snd_ctl_rawmidi_info(ctl, info); rc != 0 {
+		return nil, alsaMidiError(rc)
+	}
+	subsOut = C.snd_rawmidi_info_get_subdevices_count(info)
+
+	// List subdevices.
+	var subs C.uint
+	if subsIn > subsOut {
+		subs = subsIn
+	} else {
+		subs = subsOut
+	}
+	if subs == C.uint(0) {
+		return nil, errors.New("no streams")
+	}
+	devices := []Device{}
+
+	for sub := C.uint(0); sub < subs; sub++ {
+		subDevice, err := getSubdevice(ctl, info, card, device, sub, subsIn, subsOut)
+		if err != nil {
+			return nil, err
+		}
+		devices = append(devices, subDevice)
+	}
+	return devices, nil
 }
 
-func isMidiPort(portType C.uint) bool {
-	return (portType&C.SND_SEQ_PORT_TYPE_MIDI_GENERIC != 0) ||
-		(portType&C.SND_SEQ_PORT_TYPE_MIDI_GM != 0) ||
-		(portType&C.SND_SEQ_PORT_TYPE_MIDI_GS != 0) ||
-		(portType&C.SND_SEQ_PORT_TYPE_MIDI_XG != 0) ||
-		(portType&C.SND_SEQ_PORT_TYPE_MIDI_MT32 != 0) ||
-		(portType&C.SND_SEQ_PORT_TYPE_MIDI_GM2 != 0)
-}
-
-func isDuplexPort(caps C.uint) bool {
-	return (caps & C.SND_SEQ_PORT_CAP_DUPLEX) != 0
-}
-
-func isWritePort(caps C.uint) bool {
-	return (caps&C.SND_SEQ_PORT_CAP_WRITE != 0) ||
-		(caps&C.SND_SEQ_PORT_CAP_SYNC_WRITE != 0) ||
-		(caps&C.SND_SEQ_PORT_CAP_SUBS_WRITE != 0)
-}
-
-func isReadPort(caps C.uint) bool {
-	return (caps&C.SND_SEQ_PORT_CAP_READ != 0) ||
-		(caps&C.SND_SEQ_PORT_CAP_SYNC_READ != 0) ||
-		(caps&C.SND_SEQ_PORT_CAP_SUBS_READ != 0)
+func getSubdevice(ctl *C.snd_ctl_t, info *C.snd_rawmidi_info_t, card C.int, device, sub, subsIn, subsOut C.uint) (Device, error) {
+	if sub < subsIn {
+		C.snd_rawmidi_info_set_stream(info, C.SND_RAWMIDI_STREAM_INPUT)
+	} else {
+		C.snd_rawmidi_info_set_stream(info, C.SND_RAWMIDI_STREAM_OUTPUT)
+	}
+	C.snd_rawmidi_info_set_subdevice(info, sub)
+	if rc := C.snd_ctl_rawmidi_info(ctl, info); rc != 0 {
+		return Device{}, alsaMidiError(rc)
+	}
+	var (
+		name    = C.GoString(C.snd_rawmidi_info_get_name(info))
+		subName = C.GoString(C.snd_rawmidi_info_get_subdevice_name(info))
+	)
+	var dt DeviceType
+	if sub < subsIn && sub >= subsOut {
+		dt = DeviceInput
+	} else if sub >= subsIn && sub < subsOut {
+		dt = DeviceOutput
+	} else {
+		dt = DeviceDuplex
+	}
+	if sub == 0 && len(subName) > 0 && subName[0] == 0 {
+		return Device{
+			ID:   fmt.Sprintf("hw:%d,%d", card, device),
+			Name: name,
+			Type: dt,
+		}, nil
+	}
+	return Device{
+		ID:   fmt.Sprintf("hw:%d,%d,%d", card, device, sub),
+		Name: subName,
+		Type: dt,
+	}, nil
 }
 
 func alsaMidiError(code C.int) error {
+	if code == C.int(0) {
+		return nil
+	}
 	return errors.New(C.GoString(C.snd_strerror(code)))
 }
